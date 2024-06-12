fcb9dfa81a75193d7b9130485586fdb15c2f2888
Juan <alonso.jad@gmail.com>
2024-06-12 08:52:10 +0200

    añadidos los ejercicios

---
+++ ejercicio14.js
@@ -0,0 +1,23 @@
+const personas = [
+    {
+        "nombre": "Laura", "edad": 35,
+        "notas": [
+            { "asignatura": "Matemáticas", "nota": 8.5 },
+            { "asignatura": "Historia", "nota": 9.0 }
+        ]
+    }
+    , {
+        "nombre": "Sergio", "edad": 41,
+        "notas": [
+            { "asignatura": "Física", "nota": 7.5 },
+            { "asignatura": "Química", "nota": 8.0 }
+        ]
+    }
+    , {
+        "nombre": "Eva", "edad": 21,
+        "notas": [
+            { "asignatura": "Literatura", "nota": 9.5 },
+            { "asignatura": "Arte", "nota": 10.0 }
+        ]
+    }
+];
---
+++ ejercicio7.js
@@ -0,0 +1,27 @@
+const recetas = [
+    {
+        "nombre_receta": "Tacos de Pollo",
+        "numero_ingredientes": 10,
+        "precio": 12.50
+    },
+    {
+        "nombre_receta": "Sopa de Tomate",
+        "numero_ingredientes": 7,
+        "precio": 8.00
+    },
+    {
+        "nombre_receta": "Ensalada César",
+        "numero_ingredientes": 6,
+        "precio": 9.75
+    },
+    {
+        "nombre_receta": "Pizza Margarita",
+        "numero_ingredientes": 8,
+        "precio": 15.00
+    },
+    {
+        "nombre_receta": "Brownie de Chocolate",
+        "numero_ingredientes": 5,
+        "precio": 6.50
+    }
+];
